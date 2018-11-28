30dce0df3777cc9697fe7454226c0de99b747edb
elhache95 <45152368+elhache95@users.noreply.github.com>
2018-11-28 13:01:46 +0100

    new

---
+++ tita.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 23 11:08:33 2018
+
+@author: louis
+"""
+
+import pandas as pd
+import numpy as np
+
+data=pd.read_csv("C:/Users/louis/Documents/Msc data science/Centrale/Machine Learning/train.csv",delimiter=",")
+data
